6d345e18345b012c07b0724808cbc613447707cf
Harsh Raj Bind <121737517+harshrajbind2024@users.noreply.github.com>
2025-08-25 13:47:54 +0530

    Update 006 relation logic  operator.py

--- 006 relation logic  operator.py
+++ 006 relation logic  operator.py
@@ -9,3 +9,6 @@ print("{a} > {b}: {a > b}")     # Greater than
 print("{a} < {b}: {a < b}")     # Less than
 print("{a} >= {b}: {a >= b}")   # Greater than or equal to=0359-=
 print("{a} <= {b}: {a <= b}")   # Less than or equal to
+
+output
+# 10 == 5: False 
